74bc689c7693724ff68b23b45f8b5aa33d9371d6
Shambhu Kumar <shambhu.kumar@fridaymediagroup.com>
2018-09-14 18:01:44 +0530

    doc pubic

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Reward;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Swagger\Annotations as SWG;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class AuthenticationController
@@ -12,6 +17,24 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class AuthenticationController extends Controller
 {
+    /*
+     * @Route("/api/auth", methods={"GET"})
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns the rewards of an user",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @SWG\Items(ref=@Model(type=Reward::class, groups={"full"}))
+     *     )
+     * )
+     * @SWG\Parameter(
+     *     name="order",
+     *     in="query",
+     *     type="string",
+     *     description="The field used to order rewards"
+     * )
+     * @SWG\Tag(name="rewards")
+     */
     public function loginCheck()
     {
 //        Won't be called, it's handled by firewall
